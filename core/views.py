27e5f46245147595ffac7a6c9c8e4a471a0f2a7b
Hussein Salim <husseinsalim419@gmail.com>
2024-01-17 10:21:09 +0300

    Adding products

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from product.models import Product
+
+def frontpage(request):
+    products = Product.objects.all()[0:8]
+    return render(request, 'core/frontpage.html', {'products': products})
