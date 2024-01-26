32d1cab28207005ec97a2d9fa6eaf05b32e84a6b
Hussein Salim <husseinsalim419@gmail.com>
2024-01-26 10:29:29 +0300

    Add product images

--- core/views.py
+++ core/views.py
@@ -7,6 +7,12 @@ def frontpage(request):
     products = Product.objects.all()[0:8]
     return render(request, 'core/frontpage.html', {'products': products})
 
+def signup(request):
+    return render(request, 'core/signup.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
 def shop(request):
     categories = Category.objects.all()
     products = Product.objects.all()
